7f7afc1f7a2834b61c99687e486b72039cbbf596
P3NUT <49468484+p3nut-ai@users.noreply.github.com>
2024-11-22 19:57:13 +0800

    Update pos.py

--- pos.py
+++ pos.py
@@ -6,9 +6,9 @@ from main import *
 colorama.init()
 
 def mt5_login():
-    login = "24401976"
-    password = "lohjILDM_.42!"
-    server = "FivePercentOnline-Real"
+    login = ""
+    password = ""
+    server = ""
     mt5.login(login, password, server)
 
     return True
